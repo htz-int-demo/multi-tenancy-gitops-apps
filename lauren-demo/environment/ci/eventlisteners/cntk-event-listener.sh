530a9f16dff9cd14fa9f89a43668454ee3cedc0e
therayy <raafat@ibm.com>
2022-02-24 13:54:08 -0800

    adding htz-int

--- lauren-demo/environment/ci/eventlisteners/cntk-event-listener.sh
+++ lauren-demo/environment/ci/eventlisteners/cntk-event-listener.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Set variables
-GIT_ORG=${GIT_ORG:-client-engineering-gitops-h} 
+GIT_ORG=${GIT_ORG:-client-engineering-gitops-htz} 
 echo $GIT_ORG
 GIT_BRANCH_LAUREN=${GIT_BRANCH_LAUREN:-master}
 echo $GIT_BRANCH_LAUREN
